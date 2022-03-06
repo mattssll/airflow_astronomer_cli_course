35423e4e95d6d9ee4c18cddeaad17c6bb777f2c8
mateus.silvaleao <mateus.silvaleao@justeattakeaway.com>
2022-03-06 21:07:34 +0100

    master: task groups and xcoms in another way

---
+++ dags/g_1_taskgroups_alternative_xcoms.py
@@ -0,0 +1,23 @@
+# taskgroup.py
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.decorators import task, dag
+from airflow.utils.task_group import TaskGroup
+from groups.process_tasks import process_tasks
+from datetime import datetime
+
+@task.python(task_id="extract_partners", do_xcom_push=False, multiple_outputs=True)
+def extract():
+    partner_name = "netflix"
+    partner_path = '/partners/netflix'
+    return {"partner_name":partner_name, "partner_path":partner_path}
+
+
+@dag(schedule_interval='@daily', start_date=datetime(2022,1,1), catchup=False, max_active_runs = 1)
+def g_1_taskgroups_alternative_xcoms():
+
+    partner_settings = extract()
+
+    process_tasks(partner_settings)
+
+dag = g_1_taskgroups_alternative_xcoms()
---
+++ dags/groups/process_tasks.py
@@ -0,0 +1,43 @@
+from airflow.decorators import task, task_group
+from airflow.utils.task_group import TaskGroup
+
+@task.python
+def process_a(partner_name, partner_path):
+    print(partner_name)
+    print(partner_path)
+
+@task.python
+def process_b(partner_name, partner_path):
+    print(partner_name)
+    print(partner_path)
+
+@task.python
+def process_c(partner_name, partner_path):
+    print(partner_name)
+    print(partner_path)
+
+@task.python
+def check_a():
+    print("checking")
+
+@task.python
+def check_b():
+    print("checking")
+
+@task.python
+def check_c():
+    print("checking")
+
+
+def process_tasks(partner_settings):
+    # Two levels of task group, one task group inside another task_group
+    with TaskGroup(group_id="process_tasks") as process_tasks: # could be done with decorator too
+        with TaskGroup(group_id='test_tasks') as test_tasks:
+            check_a()
+            check_b()
+            check_c()
+        process_a(partner_settings['partner_name'], partner_settings['partner_path']) >> test_tasks
+        process_b(partner_settings['partner_name'], partner_settings['partner_path']) >> test_tasks
+        process_c(partner_settings['partner_name'], partner_settings['partner_path']) >> test_tasks
+    return process_tasks
+
